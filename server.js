55c96cdeadb9eda5aa742c313d3aad531a6b6b59
Sonia Gómez Rojas <sogoro@MacBook-Air-de-Sonia.local>
2025-10-21 22:06:38 +0200

    Servir index.html en la raíz

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
+app.use(express.static('public'));
+// Servir index.html al acceder a la raíz
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 app.use(express.json());
 
 // ----------------------
