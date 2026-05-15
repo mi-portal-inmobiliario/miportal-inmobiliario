f754f4853f90792d105bc89c47c9ee0c091a2a2d
Sonia Gómez Rojas <sogoro@MacBook-Air-de-Sonia.local>
2026-05-15 15:33:34 +0200

    Implementar banner cookies global

---
+++ public/js/cookies.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  // comprobar si ya aceptó
+  const aceptadas = localStorage.getItem("cookiesAceptadas");
+
+  if (aceptadas) return;
+
+  // crear banner
+  const banner = document.createElement("div");
+
+  banner.innerHTML = `
+    <div class="cookie-box">
+      <p>
+        HomeClick24 utiliza cookies para mejorar tu experiencia.
+        <a href="/cookies.html">Más información</a>
+      </p>
+
+      <button id="aceptarCookies">
+        Aceptar
+      </button>
+    </div>
+  `;
+
+  banner.classList.add("cookie-banner");
+
+  document.body.appendChild(banner);
+
+  // aceptar
+  document
+    .getElementById("aceptarCookies")
+    .addEventListener("click", () => {
+
+      localStorage.setItem("cookiesAceptadas", "true");
+
+      banner.remove();
+    });
+
+});
